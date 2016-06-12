6eb7f8b7cef058e16c99cfcd55e5f6473872b2c6
mo802e08 <mo802e08@panama.redes.lab.ic.unicamp.br>
2016-06-12 14:58:49 -0300

    correcoes nas datas do relatorio

--- js/consignacao_relatorio.js
+++ js/consignacao_relatorio.js
@@ -9,18 +9,15 @@
         return {
             getAll:function() {
                 return $http.get('https://localhost:8443/purplefitness/rest/consignment/search');
+            },
+            filter: function (dataInicio, dataFim) {
+              return $http.get('https://localhost:8443/purplefitness/rest/consignment/search?di=' + dataInicio + 'df=' + dataFim);  
             }
         };
     })
 
 	.controller('consignacaoController',function($scope, Consignacao, toastr){ 
         //consignação
-        /*Consignacao.getAll().then(function(response) {
-            $scope.consignacoes = response.data;
-            console.log($scope.consignacoes);
-        }, function(response) {
-            console.log(response);
-        });*/
         $scope.consignacoes_itens = [ 
              {nameCustomer:'Maria Hosé',finalDate:'14/04/2016', nameProduct:'Nome', quantity_consig:'10', quantity_used:'10', price:'15'},
              {nameCustomer:'Maria Hosé',finalDate:'14/04/2016', nameProduct:'Nome', quantity_consig:'10', quantity_used:'10', price:'15'},
@@ -29,11 +26,24 @@
              {nameCustomer:'Maria Hosé',finalDate:'14/04/2016', nameProduct:'Nome', quantity_consig:'10', quantity_used:'10', price:'15'}
          ];
 
+         $scope.relatorio = {};
+         var now = new Date();
+         $scope.relatorio.dataInicial = ('00' + now.getDate()).substr(-2) + '/' + ('00' + (now.getMonth() + 1)).substr(-2) + '/' + now.getFullYear();
+
+         now.setMonth(now.getMonth() - 1);
+         $scope.relatorio.dataFinal = ('00' + now.getDate()).substr(-2) + '/' + ('00' + (now.getMonth() + 1)).substr(-2) + '/' + now.getFullYear();
+
        $scope.gerar_relatorio = function(relatorio){ 
             $scope.relatorioForm.$setPristine(); 
             $scope.relatorioForm.$setUntouched();
-            $scope.exibir_relatorio=1;
-            console.log($scope.relatorio);         
+            
+            // Consignacao.filter($scope.relatorio.dataInicial, $scope.relatorio.dataFinal).then(function(response) {
+            //     $scope.consignacoes = response.data;
+                $scope.exibir_relatorio=1;
+            //     //console.log($scope.consignacoes);
+            // }, function(response) {
+            //     // console.log(response);
+            // });
         }
     });
 })();
